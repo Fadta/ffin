21dbf8f025107460be4d2dc6b3bba4d0b047b90a
Fadta <fvpan01@gmail.com>
2021-12-30 01:28:49 -0300

    Initial commit, structure

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fadta Finance
+
+A Python module built in Python 3.9
+
+
+## Tested Python versions
+
+	-[x] 3.9
+	-[ ] 3.8
+	-[ ] 3.7
+	-[ ] 3.6
---
+++ setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+
+setup(
+    # Application name:
+    name="ffin",
+
+    version="0.0.1",
+
+    author="Francisco Vicente",
+    author_email="fvpan01@gmail.com",
+
+    # Packages
+    packages=["ffin"],
+
+    # Include additional files into the package
+    include_package_data=True,
+
+    # Details
+    url="https://github.com/Fadta/ffin",
+
+    #
+    license="LICENSE",
+    # description="",
+
+    # long_description=open("README.md").read(),
+
+    # Dependent packages (distributions)
+    install_requires=[
+        "pandas",
+        "yfinance",
+    ],
+
+    entry_points={
+        "console_scripts": [
+        ],
+    }
+)
